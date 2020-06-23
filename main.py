7498fb14590ba2cb895daee7d33702567b402573
leynier <leynier41@gmail.com>
2020-06-23 01:51:30 -0400

    Implement fetch of JSON from application repository

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from hashlib import sha1
 from json import dump
+from requests import get
 
 
 def main():
     result = {}
+    json = get('https://raw.githubusercontent.com/todo-devs/todo/master/config/ussd_codes.json')
+    with open('config.json', mode='w', encoding='utf-8') as file:
+        file.write(json.text)
     with open('config.json', encoding='utf-8') as file:
         text = file.read()
         cache = sha1(text.encode())
